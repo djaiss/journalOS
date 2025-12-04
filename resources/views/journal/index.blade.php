9c73c678d46935bdba78260b293d36a6d5197771
Regis Freyd <djaiss@users.noreply.github.com>
2025-12-04 08:53:53 -0700

    wip

--- resources/views/journal/index.blade.php
+++ resources/views/journal/index.blade.php
@@ -29,7 +29,7 @@
           <div class="flex flex-col items-center gap-2 p-4 text-center text-gray-500">
             <div class="mb-1 rounded-full bg-gray-100 p-3">
               <x-phosphor-building-office class="size-6 text-gray-600" />
-            </div
+            </div>
 
             <p class="text-sm text-gray-500">
               {{ __('You do not have any journals yet.') }}
