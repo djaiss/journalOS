00aea69a3f4f98653fedc2aff72bb8b7ce6e9fc2
Regis Freyd <djaiss@users.noreply.github.com>
2025-12-29 11:13:33 -0500

    docs: add journal entry api doc (#68)

---
+++ resources/views/marketing/docs/api/partials/journal-entry-response.blade.php
@@ -0,0 +1,70 @@
+<div>{</div>
+<div class="pl-4">"data": {</div>
+<div class="pl-8">
+  "type":
+  <span class="text-lime-700">"journal_entry"</span>
+  ,
+</div>
+<div class="pl-8">
+  "id":
+  <span class="text-rose-800">"442"</span>
+  ,
+</div>
+<div class="pl-8">"attributes": {</div>
+<div class="pl-12">
+  "journal_id":
+  <span class="text-rose-800">663</span>
+  ,
+</div>
+<div class="pl-12">
+  "day":
+  <span class="text-rose-800">18</span>
+  ,
+</div>
+<div class="pl-12">
+  "month":
+  <span class="text-rose-800">6</span>
+  ,
+</div>
+<div class="pl-12">
+  "year":
+  <span class="text-rose-800">2024</span>
+  ,
+</div>
+<div class="pl-12">
+  "modules": {</div>
+<div class="pl-16">"sleep": {</div>
+<div class="pl-20">
+  "bedtime":
+  <span class="text-lime-700">"22:30"</span>
+  ,
+</div>
+<div class="pl-20">
+  "wake_up_time":
+  <span class="text-lime-700">"06:45"</span>
+  ,
+</div>
+<div class="pl-20">
+  "sleep_duration_in_minutes":
+  <span class="text-rose-800">495</span>
+</div>
+<div class="pl-16">}</div>
+<div class="pl-12">},</div>
+<div class="pl-12">
+  "created_at":
+  <span class="text-rose-800">1751305720</span>
+  ,
+</div>
+<div class="pl-12">
+  "updated_at":
+  <span class="text-rose-800">1751305720</span>
+</div>
+<div class="pl-8">},</div>
+<div class="pl-8">"links": {</div>
+<div class="pl-12">
+  "self":
+  <span class="text-lime-700">"{{ config('app.url') }}/api/journals/663/2024/6/18"</span>
+</div>
+<div class="pl-8">}</div>
+<div class="pl-4">}</div>
+<div>}</div>
