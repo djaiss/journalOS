3163bfdc9d834eb7a70642dfeb7eec100161616b
Regis Freyd <djaiss@users.noreply.github.com>
2025-12-29 08:26:35 -0500

    wip

---
+++ resources/views/app/journal/settings/partials/delete.blade.php
@@ -0,0 +1,15 @@
+<x-box>
+  <x-slot:title>
+    {{ __('Delete journal') }}
+  </x-slot>
+
+  <x-slot:description>
+    {{ __('This will delete the journal and all the related journal entries.') }}
+  </x-slot>
+
+  <x-form onsubmit="return confirm('Are you absolutely sure? This action cannot be undone.');" action="{{ route('journal.destroy', ['slug' => $journal->slug]) }}" method="delete">
+    <x-button.danger type="submit" class="text-sm">
+      {{ __('Delete journal') }}
+    </x-button.danger>
+  </x-form>
+</x-box>
