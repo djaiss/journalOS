4ec2f885f74c7b64f4006c3fe9c13239cb1051d1
Regis Freyd <djaiss@users.noreply.github.com>
2025-12-30 22:49:27 -0500

    Update modules.blade.php

--- resources/views/app/journal/settings/partials/modules.blade.php
+++ resources/views/app/journal/settings/partials/modules.blade.php
@@ -8,7 +8,7 @@
   </x-slot>
 
   <!-- sleep module -->
-  <x-form method="put" action="{{ route('journal.settings.modules.update', ['slug' => $journal->slug]) }}" x-target="modules-container notifications" x-target.back="modules-container" id="modules-form">
+  <x-form method="put" action="{{ route('journal.settings.modules.update', ['slug' => $journal->slug]) }}" x-target="modules-container notifications" x-target.back="modules-container" id="sleep-module-form">
     <input type="hidden" name="module" value="sleep" />
     <div class="grid grid-cols-3 items-center rounded-t-lg p-3 hover:bg-blue-50 dark:hover:bg-gray-800 border-b border-gray-200 dark:border-gray-700">
       <p class="col-span-2 block text-sm font-medium text-gray-700 dark:text-gray-300">{{ __('Sleep module') }}</p>
@@ -19,7 +19,7 @@
   </x-form>
 
   <!-- work module -->
-  <x-form method="put" action="{{ route('journal.settings.modules.update', ['slug' => $journal->slug]) }}" x-target="modules-container notifications" x-target.back="modules-container" id="modules-form">
+  <x-form method="put" action="{{ route('journal.settings.modules.update', ['slug' => $journal->slug]) }}" x-target="modules-container notifications" x-target.back="modules-container" id="work-module-form">
     <input type="hidden" name="module" value="work" />
     <div class="grid grid-cols-3 items-center rounded-b-lg p-3 hover:bg-blue-50 dark:hover:bg-gray-800">
       <p class="col-span-2 block text-sm font-medium text-gray-700 dark:text-gray-300">{{ __('Work module') }}</p>
