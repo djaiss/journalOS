2c6e5883e881ef0fee5d2a2c58d1b44f77ae2b4f
Regis Freyd <djaiss@users.noreply.github.com>
2025-12-30 19:01:21 -0500

    feat: year selector

--- tests/Unit/Helpers/JournalHelperTest.php
+++ tests/Unit/Helpers/JournalHelperTest.php
@@ -6,6 +6,7 @@ namespace Tests\Unit\Helpers;
 
 use App\Helpers\JournalHelper;
 use App\Models\Journal;
+use App\Models\JournalEntry;
 use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
@@ -14,6 +15,64 @@ use PHPUnit\Framework\Attributes\Test;
 final class JournalHelperTest extends TestCase
 {
     use RefreshDatabase;
+
+    #[Test]
+    public function it_gets_all_distinct_years_from_journal_entries(): void
+    {
+        $journal = Journal::factory()->create();
+
+        JournalEntry::factory()->create([
+            'journal_id' => $journal->id,
+            'year' => 2023,
+        ]);
+        JournalEntry::factory()->create([
+            'journal_id' => $journal->id,
+            'year' => 2023,
+        ]);
+        JournalEntry::factory()->create([
+            'journal_id' => $journal->id,
+            'year' => 2024,
+        ]);
+        JournalEntry::factory()->create([
+            'journal_id' => $journal->id,
+            'year' => 2022,
+        ]);
+
+        $years = JournalHelper::getYears(
+            journal: $journal,
+            selectedYear: 2023,
+        );
+
+        $this->assertCount(3, $years);
+        $this->assertEquals((object) [
+            'year' => 2024,
+            'is_selected' => false,
+            'url' => env('APP_URL') . '/journals/' . $journal->slug . '/entries/2024/1/1',
+        ], $years[2024]);
+        $this->assertEquals((object) [
+            'year' => 2023,
+            'is_selected' => true,
+            'url' => env('APP_URL') . '/journals/' . $journal->slug . '/entries/2023/1/1',
+        ], $years[2023]);
+        $this->assertEquals((object) [
+            'year' => 2022,
+            'is_selected' => false,
+            'url' => env('APP_URL') . '/journals/' . $journal->slug . '/entries/2022/1/1',
+        ], $years[2022]);
+    }
+
+    #[Test]
+    public function it_returns_empty_collection_when_journal_has_no_entries(): void
+    {
+        $journal = Journal::factory()->create();
+
+        $years = JournalHelper::getYears(
+            journal: $journal,
+            selectedYear: 2023,
+        );
+
+        $this->assertCount(0, $years);
+    }
 
     #[Test]
     public function it_gets_all_months_in_a_given_year(): void
