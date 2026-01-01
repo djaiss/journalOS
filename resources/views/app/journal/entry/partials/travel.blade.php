bec896fb94432cbf4c930c2dc2ddfb29f8618f17
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-01 10:45:57 -0500

    feat: update travel module to show travel details conditionally

--- resources/views/app/journal/entry/partials/travel.blade.php
+++ resources/views/app/journal/entry/partials/travel.blade.php
@@ -13,30 +13,34 @@
     </div>
   </x-slot>
 
-  <div id="travel-container" class="space-y-4">
-    <div class="space-y-4">
+  <div id="travel-container" class="space-y-4" x-data="{
+    showTravelDetails: {{ $entry->has_traveled_today === 'yes' ? 'true' : 'false' }},
+  }">
+    <div>
       <!-- Have you traveled today? -->
       <div class="space-y-2">
         <p>{{ __('Have you traveled today?') }}</p>
         <div class="flex w-full rounded-lg border border-gray-200 dark:border-gray-700">
-          <x-button.yes name="has_traveled" value="yes" x-target="travel-container notifications travel-reset" :action="$module['has_traveled_url']" selected="{{ $entry->has_traveled_today === 'yes' }}" />
-          <x-button.no name="has_traveled" value="no" x-target="travel-container notifications travel-reset" :action="$module['has_traveled_url']" selected="{{ $entry->has_traveled_today === 'no' }}" />
+          <x-button.yes name="has_traveled" value="yes" x-target="travel-container notifications travel-reset" :action="$module['has_traveled_url']" selected="{{ $entry->has_traveled_today === 'yes' }}" @click="showTravelDetails = true" />
+          <x-button.no name="has_traveled" value="no" x-target="travel-container notifications travel-reset" :action="$module['has_traveled_url']" selected="{{ $entry->has_traveled_today === 'no' }}" @click="showTravelDetails = false" />
         </div>
       </div>
 
-      <!-- Travel mode -->
-      <div class="space-y-2">
-        <p>{{ __('How did you travel?') }}</p>
-        <x-form x-target="travel-container notifications travel-reset" :action="$module['travel_mode_url']" method="put" id="travel-mode-form">
-          <div class="grid grid-cols-4 gap-2">
-            @foreach ($module['travel_modes'] as $option)
-              <label class="{{ $option['is_selected'] ? 'bg-green-50 font-bold dark:bg-green-900/40' : '' }} flex cursor-pointer items-center justify-center rounded-lg border border-gray-200 p-2 text-center hover:bg-green-50 dark:border-gray-700 dark:hover:bg-green-900/40">
-                <input type="checkbox" name="travel_modes[]" value="{{ $option['value'] }}" {{ $option['is_selected'] ? 'checked' : '' }} onchange="document.getElementById('travel-mode-form').requestSubmit()" class="hidden" />
-                <span>{{ $option['label'] }}</span>
-              </label>
-            @endforeach
-          </div>
-        </x-form>
+      <div x-show="showTravelDetails" x-cloak class="mt-4 space-y-4">
+        <!-- Travel mode -->
+        <div class="space-y-2">
+          <p>{{ __('How did you travel?') }}</p>
+          <x-form x-target="travel-container notifications travel-reset" :action="$module['travel_mode_url']" method="put" id="travel-mode-form">
+            <div class="grid grid-cols-4 gap-2">
+              @foreach ($module['travel_modes'] as $option)
+                <label class="{{ $option['is_selected'] ? 'bg-green-50 font-bold dark:bg-green-900/40' : '' }} flex cursor-pointer items-center justify-center rounded-lg border border-gray-200 p-2 text-center hover:bg-green-50 dark:border-gray-700 dark:hover:bg-green-900/40">
+                  <input type="checkbox" name="travel_modes[]" value="{{ $option['value'] }}" {{ $option['is_selected'] ? 'checked' : '' }} onchange="document.getElementById('travel-mode-form').requestSubmit()" class="hidden" />
+                  <span>{{ $option['label'] }}</span>
+                </label>
+              @endforeach
+            </div>
+          </x-form>
+        </div>
       </div>
     </div>
   </div>
