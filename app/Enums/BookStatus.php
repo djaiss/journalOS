d1bc1a45561df69aaae994c9083ea96cca25da57
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-04 11:03:34 -0500

    feat: add book model

---
+++ app/Enums/BookStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Enums;
+
+enum BookStatus: string
+{
+    case STARTED = 'started';
+    case CONTINUED = 'continued';
+    case FINISHED = 'finished';
+}
