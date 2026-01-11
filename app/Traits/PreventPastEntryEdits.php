aa55fc0d4ed640fde55ae95d13d972b9612f0e1c
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-11 18:12:16 -0500

    Update PreventPastEntryEdits.php

--- app/Traits/PreventPastEntryEdits.php
+++ app/Traits/PreventPastEntryEdits.php
@@ -12,9 +12,10 @@ trait PreventPastEntryEdits
 {
     private function preventPastEditsAllowed(JournalEntry $entry, int $days = 7): void
     {
-        if ($this->entry->journal->can_edit_past === false) {
-            $sevenDaysAgo = now()->subDays(7)->startOfDay();
-            $entryDate = Carbon::create($this->entry->year, $this->entry->month, $this->entry->day)->startOfDay();
+        if ($entry->journal->can_edit_past === false) {
+            $sevenDaysAgo = now()->subDays($days)->startOfDay();
+
+            $entryDate = Carbon::create($entry->year, $entry->month, $entry->day)->startOfDay();
             if ($entryDate->lt($sevenDaysAgo)) {
                 throw new ModelNotFoundException('Editing past entries is not allowed for this journal.');
             }
