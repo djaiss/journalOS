4b9c66a5e732b1d8f17d2eee618077c99bc9373c
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-13 09:57:59 -0500

    wip

--- resources/views/marketing/index.blade.php
+++ resources/views/marketing/index.blade.php
@@ -17,7 +17,7 @@
         "url": "https://journalos.cloud",
         "applicationCategory": "BusinessApplication",
         "operatingSystem": "All",
-        "logo": "https://journalos.cloud/marketing/logo-full.png",
+        "logo": "https://journalos.cloud/images/marketing/logo-full.png",
         "softwareHelp": "https://journalos.cloud/docs",
         "offers": {
           "@type": "Offer",
@@ -29,7 +29,7 @@
           "@type": "Organization",
           "name": "JournalOS",
           "url": "https://journalos.cloud",
-          "logo": "https://journalos.cloud/marketing/logo-full.png"
+          "logo": "https://journalos.cloud/images/marketing/logo-full.png"
         },
         "description": "JournalOS is a personal CRM software that helps you remember and manage your relationships.",
         "inLanguage": "en",
@@ -246,7 +246,7 @@
         <div class="flex items-center lg:col-span-1 lg:items-start">
           <div class="flex flex-col " x-data="{ isRotating: false }">
             <div class="relative">
-              <x-image src="{{ asset('marketing/regis.webp') }}" srcset="{{ asset('marketing/regis.webp') }} 1x, {{ asset('marketing/regis@2x.webp') }} 2x" alt="Monica" class="mb-3 w-40 rounded-lg transition-all duration-[2000ms] ease-[cubic-bezier(0.34,1.56,0.64,1)] hover:scale-110 hover:rotate-[360deg] lg:rotate-4" @mouseenter="isRotating = true" @mouseleave="isRotating = false" @transitionend="isRotating = false" width="187" height="187" />
+              <x-image src="{{ asset('images/marketing/regis.webp') }}" srcset="{{ asset('images/marketing/regis.webp') }} 1x, {{ asset('images/marketing/regis@2x.webp') }} 2x" alt="Monica" class="mb-3 w-40 rounded-lg transition-all duration-[2000ms] ease-[cubic-bezier(0.34,1.56,0.64,1)] hover:scale-110 hover:rotate-[360deg] lg:rotate-4" @mouseenter="isRotating = true" @mouseleave="isRotating = false" @transitionend="isRotating = false" width="187" height="187" />
 
               <!-- Tooltip -->
                 <div x-show="isRotating" x-transition.opacity class="bg-opacity-75 absolute top-1/2 right-full mr-3 -translate-y-1/2 rounded-lg bg-black px-3 py-2 text-sm whitespace-nowrap text-white">Please stooooop this! 😵‍💫</div>
@@ -312,7 +312,7 @@
         <div class="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-4">
           <div class="rotate-2 rounded-lg border border-gray-200 bg-white p-4">
             <div class="mb-2 flex items-center justify-center">
-              <x-image src="{{ asset('marketing/good_memory.webp') }}" srcset="{{ asset('marketing/good_memory.webp') }} 1x, {{ asset('marketing/good_memory@2x.webp') }} 2x" alt="Good memory" width="167" height="250" />
+              <x-image src="{{ asset('images/marketing/good_memory.webp') }}" srcset="{{ asset('images/marketing/good_memory.webp') }} 1x, {{ asset('images/marketing/good_memory@2x.webp') }} 2x" alt="Good memory" width="167" height="250" />
             </div>
             <p class="mb-3 text-xl">You have a good memory</p>
             <p class="text-sm">If you can remember everything about everyone you know, you probably don't need this tool.</p>
@@ -320,7 +320,7 @@
 
           <div class="-rotate-1 rounded-lg border border-gray-200 bg-white p-4">
             <div class="mb-2 flex items-center justify-center">
-              <x-image src="{{ asset('marketing/recurring.webp') }}" srcset="{{ asset('marketing/recurring.webp') }} 1x, {{ asset('marketing/recurring@2x.webp') }} 2x" alt="Expensive subscriptions" width="167" height="250" />
+              <x-image src="{{ asset('images/marketing/recurring.webp') }}" srcset="{{ asset('images/marketing/recurring.webp') }} 1x, {{ asset('images/marketing/recurring@2x.webp') }} 2x" alt="Expensive subscriptions" width="167" height="250" />
             </div>
             <p class="mb-3 text-xl">You like expensive, recurring subscriptions</p>
             <p class="text-sm">We offer a one-time payment for the software. No subscriptions, no hidden fees.</p>
@@ -328,7 +328,7 @@
 
           <div class="-rotate-1 rounded-lg border border-gray-200 bg-white p-4">
             <div class="mb-2 flex items-center justify-center">
-              <x-image src="{{ asset('marketing/ads.webp') }}" srcset="{{ asset('marketing/ads.webp') }} 1x, {{ asset('marketing/ads@2x.webp') }} 2x" alt="Ads" width="167" height="250" />
+              <x-image src="{{ asset('images/marketing/ads.webp') }}" srcset="{{ asset('images/marketing/ads.webp') }} 1x, {{ asset('images/marketing/ads@2x.webp') }} 2x" alt="Ads" width="167" height="250" />
             </div>
             <p class="mb-3 text-xl">You like being tracked for ads purposes</p>
             <p class="text-sm">We don't track users to serve ads, and don't profile our users. We hate ads as much as you do.</p>
@@ -336,7 +336,7 @@
 
           <div class="rotate-2 rounded-lg border border-gray-200 bg-white p-4">
             <div class="mb-2 flex items-center justify-center">
-              <x-image src="{{ asset('marketing/prison.webp') }}" srcset="{{ asset('marketing/prison.webp') }} 1x, {{ asset('marketing/prison@2x.webp') }} 2x" alt="Prison" width="167" height="250" />
+              <x-image src="{{ asset('images/marketing/prison.webp') }}" srcset="{{ asset('images/marketing/prison.webp') }} 1x, {{ asset('images/marketing/prison@2x.webp') }} 2x" alt="Prison" width="167" height="250" />
             </div>
             <p class="mb-3 text-xl">You like being locked in</p>
             <p class="text-sm">We strongly advocate that you don't use our hosted version, and that you self-host the software on a server of your own.</p>
