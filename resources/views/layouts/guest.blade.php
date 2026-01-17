984531ec4ed924de4fd00f217f08681e4b1dfef5
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-17 17:03:36 -0500

    fix: fix turnstile script

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -10,12 +10,12 @@
   <head>
     @include('components.meta', ['title' => $title ?? null])
 
+    @if (config('journalos.show_marketing_site'))
+      <script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
+    @endif
+
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    @if (config('journalos.show_marketing_site'))
-      @turnstileScripts()
-    @endif
   </head>
   <body class="font-sans text-gray-900 antialiased dark:bg-gray-950 dark:text-gray-100">
     <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
