b655ce16655861513f1474ffd80938403e3ab70c
Regis Freyd <djaiss@users.noreply.github.com>
2026-01-19 20:34:45 -0500

    Update FeaturesControllerTest.php

--- tests/Feature/Controllers/Marketing/Features/FeaturesControllerTest.php
+++ tests/Feature/Controllers/Marketing/Features/FeaturesControllerTest.php
@@ -8,12 +8,12 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-final class MarketingControllerTest extends TestCase
+final class FeaturesControllerTest extends TestCase
 {
     use RefreshDatabase;
 
     #[Test]
-    public function it_renders_the_marketing_homepage(): void
+    public function it_renders_the_modules_catalog(): void
     {
         $response = $this->get(route('marketing.features.modules', absolute: false));
 
